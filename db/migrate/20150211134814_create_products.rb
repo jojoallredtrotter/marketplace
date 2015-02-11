23fb80edf56e59373b69104ec55e5f9e4aaa6283
Joverne Trotter <jojotrotter@gmail.com>
2015-02-11 09:09:22 -0500

    first commit

---
+++ db/migrate/20150211134814_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :category
+      t.string :name
+      t.text :description
+      t.float :price
+      t.integer :quantity
+
+      t.timestamps null: false
+    end
+  end
+end
